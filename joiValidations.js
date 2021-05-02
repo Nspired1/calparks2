d4035cfebd67eca963a98d8a6291bc6a7da76224
Nspired1 <don.spire1@gmail.com>
2021-05-02 16:13:28 -0700

    update joi validations

--- joiValidations.js
+++ joiValidations.js
@@ -1,5 +1,8 @@
 const Joi = require("joi");
 
+//file is called joiValidations, though this is joi Validations SCHEMA. Validation code is in app.js file,
+// but named this file joiValidations to avoid confusion with schema files in models.
+
 module.exports.parkSchema = Joi.object({
   park: Joi.object({
     title: Joi.string().required(),
