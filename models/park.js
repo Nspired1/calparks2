fb325f856bb9a23af34d514a0273b2f130a9dcd4
Nspired1 <don.spire1@gmail.com>
2021-05-04 16:13:43 -0700

    add virtual thumbnail property

--- models/park.js
+++ models/park.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+//virtual schema for image thumbnails to be displayed on SHOW page
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("upload", "upload/w_300");
+});
+
 const ParkSchema = new Schema({
   title: String,
   price: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   description: String,
   location: String,
 });
